d7853d647d7b233258b50eefb9539b770bb017ca
avram plihal <avramplihal@avrams-MacBook-Pro.local>
2026-09-19 17:39:54 -0400

    Added pedropathing & set up shell of the program

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -1,8 +1,15 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.pedropathing.follower.Follower;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 public class Robot {
     DriveTrain driveTrain;
     Shooter shooter;
     Transfer transfer;
+    Follower follower;
 
+    public Robot(HardwareMap hwMap){
+
+    }
 }

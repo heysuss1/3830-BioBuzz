e1d524fe30f2a14259d14803d8c6df92d0c8c4ea
Cade Gebo <cadegebo@gmail.com>
2026-09-24 17:20:34 -0400

    Made a PIDFF conreoller, made shot calculations, updated flywheel to use pidff controller.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/math/ShotCalculator.java
+++
@@ -1,9 +0,0 @@
-package org.firstinspires.ftc.teamcode.math;
-
-
-/*When shooting on the move code is implemented, it will go here, accounting
-for robots velocity and everything.*/
-//An object of this will be created in the robot class
-public class ShotCalculator {
-
-}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Flywheel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Flywheel.java
@@ -1,21 +1,18 @@
 package org.firstinspires.ftc.teamcode.subsystems;
+
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.teamcode.controllers.PIDFF;
 
 public class Flywheel {
     private final DcMotorEx flyMotor;
     private final ElapsedTime timer;
-
-    private double kP = 0.0; // proporional
-    private double kD = 0.0; // derivitive
-    private double kK = 0.0; // kinetic friction
-    private double kS = 0.0; // static friction
-
+    private final PIDFF pidff = new PIDFF(0,0,0,0,0,0,0,);
     private double targetRPM = 0.0;
-    private double lastError = 0.0;
     private double lastTime = 0.0;
     private static final double TICKS_PER_REV = 28.0;
+
     public Flywheel(HardwareMap hwMap){
         this.flyMotor = hwMap.get(DcMotorEx.class, "flywheel");
         this.flyMotor.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
@@ -23,28 +20,21 @@ public class Flywheel {
         this.timer = new ElapsedTime();
         this.timer.reset();
     }
-    public void setCoefficients(double kP, double kD, double kK, double kS) {
-        this.kP = kP;
-        this.kD = kD;
-        this.kK = kK;
-        this.kS = kS;
-    }
+
     public void setTargetRPM(double targetRPM) {
         this.targetRPM = targetRPM;
     }
+
     public double getRPM() {
         double ticksPerSecond = flyMotor.getVelocity();
         return ticksPerSecond*60.0/TICKS_PER_REV;
     }
+
     public void updateFly(){
-        //feedback (proportional and derivitive) and feedforeward (kinetic and static)
-        double time = timer.seconds();
-        double error = targetRPM - getRPM();
-        double errorDerivitive = (error-lastError) / (time-lastTime);
-        double totalPower = kP*error + kD*errorDerivitive + kK*targetRPM + kS;
+        double dt = timer.seconds() - lastTime;
+        lastTime = timer.seconds();
+        double totalPower = pidff.calcPIDFF(getRPM(), targetRPM, dt, 0,0);
         totalPower = Math.min(Math.max(totalPower, -1), 1); //clip power between -1 and 1
         flyMotor.setPower(totalPower);
-        lastError = error;
-        lastTime = time;
     }
 }

ca6cc77d3e04e687b00fa8b6616e52fe955d5350
Amelia <acleopanda@gmail.com>
2026-09-25 16:00:38 -0400

    Field and robot centric methods added in robot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -1,7 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.pedropathing.drivetrain.DrivePowers;
 import com.pedropathing.follower.Follower;
+import com.pedropathing.follower.ManualDrive;
 import com.pedropathing.math.Pose;
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.subsystems.Drive;
@@ -18,6 +21,7 @@ public class Robot {
     Transfer transfer;
 
     Follower follower;
+
     public static Pose teleOpStartPose;
 
     public Robot(HardwareMap hwMap){
@@ -26,5 +30,23 @@ public class Robot {
         flywheel = new Flywheel(hwMap);
         turret = new Turret(hwMap);
         transfer = new Transfer(hwMap);
+    }
+    public void setRobotCentricDriving(Gamepad  gamepad1){
+        follower.manual(
+        -gamepad1.left_stick_y,
+        gamepad1.left_stick_x,
+        gamepad1.right_stick_x
+        );
+
+    }
+    public void setFieldCentricDriving(Gamepad gamepad1){
+        DrivePowers powers = ManualDrive.fieldCentric(
+                -gamepad1.left_stick_y,
+                gamepad1.left_stick_x,
+                gamepad1.right_stick_x,
+                follower.pose().heading()
+        );
+        follower.manual(powers);
+
     }
 }
